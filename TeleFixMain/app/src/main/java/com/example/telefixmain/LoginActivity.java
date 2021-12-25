a896c497e2c05cca125685963c892e822542b1e1
Andrew Le <s3820098@rmit.edu.vn>
2021-12-25 20:44:34 +0700

    An 10: implement password toggle

--- TeleFixMain/app/src/main/java/com/example/telefixmain/LoginActivity.java
+++ TeleFixMain/app/src/main/java/com/example/telefixmain/LoginActivity.java
@@ -2,16 +2,24 @@ package com.example.telefixmain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.animation.AnimationUtils;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+@SuppressLint("ClickableViewAccessibility")
 public class LoginActivity extends AppCompatActivity {
     LinearLayout llLogin;
     TextView jumpToSignup;
+    EditText pwdLogin;
+    boolean pwdIsVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,31 @@ public class LoginActivity extends AppCompatActivity {
         // login contents fade in
         llLogin = findViewById(R.id.ll_login);
         llLogin.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+
+        // toggle password
+        pwdLogin = findViewById(R.id.pwd_login);
+        pwdLogin.setOnTouchListener((view, motionEvent) -> {
+            final int right = 2;
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                if (motionEvent.getRawX() >= pwdLogin.getRight() - pwdLogin.getCompoundDrawables()[right].getBounds().width()) {
+                    int selection = pwdLogin.getSelectionEnd();
+
+                    if (pwdIsVisible) {
+                        pwdLogin.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_login_pwd, 0, R.drawable.ic_pwd_visibility_off, 0);
+                        pwdLogin.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                        pwdIsVisible = false;
+                    } else {
+                        pwdLogin.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_login_pwd, 0, R.drawable.ic_pwd_visibility, 0);
+                        pwdLogin.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                        pwdIsVisible = true;
+                    }
+
+                    pwdLogin.setSelection(selection);
+                    return true;
+                }
+            }
+            return false;
+        });
 
         // jump tp Sign Up
         jumpToSignup = findViewById(R.id.jump_to_signup);
--- TeleFixMain/app/src/main/java/com/example/telefixmain/SignUpActivity.java
+++ TeleFixMain/app/src/main/java/com/example/telefixmain/SignUpActivity.java
@@ -2,16 +2,24 @@ package com.example.telefixmain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.animation.AnimationUtils;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+@SuppressLint("ClickableViewAccessibility")
 public class SignUpActivity extends AppCompatActivity {
     LinearLayout llSignup;
     TextView jumpToLogin;
+    EditText pwdSignup;
+    boolean pwdIsVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,31 @@ public class SignUpActivity extends AppCompatActivity {
         // sign up contents fade in
         llSignup = findViewById(R.id.ll_signup);
         llSignup.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+
+        // toggle password
+        pwdSignup = findViewById(R.id.pwd_signup);
+        pwdSignup.setOnTouchListener((view, motionEvent) -> {
+            final int right = 2;
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                if (motionEvent.getRawX() >= pwdSignup.getRight() - pwdSignup.getCompoundDrawables()[right].getBounds().width()) {
+                    int selection = pwdSignup.getSelectionEnd();
+
+                    if (pwdIsVisible) {
+                        pwdSignup.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_login_pwd, 0, R.drawable.ic_pwd_visibility_off, 0);
+                        pwdSignup.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                        pwdIsVisible = false;
+                    } else {
+                        pwdSignup.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_login_pwd, 0, R.drawable.ic_pwd_visibility, 0);
+                        pwdSignup.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                        pwdIsVisible = true;
+                    }
+
+                    pwdSignup.setSelection(selection);
+                    return true;
+                }
+            }
+            return false;
+        });
 
         // jump tp Login
         jumpToLogin = findViewById(R.id.jump_to_login);
