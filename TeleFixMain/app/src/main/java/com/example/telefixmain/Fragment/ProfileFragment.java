6a60ea7384cf193d7bea5765de5b4e569f01cec2
Andrew Le <s3820098@rmit.edu.vn>
2021-12-29 22:57:57 +0700

    An 43: reduce lagging when going to Profile Fragment

--- TeleFixMain/app/src/main/java/com/example/telefixmain/Fragment/ProfileFragment.java
+++ TeleFixMain/app/src/main/java/com/example/telefixmain/Fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.telefixmain.Fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 import com.example.telefixmain.R;
 
 public class ProfileFragment extends Fragment {
+    LinearLayout profileContent;
+
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -18,7 +23,20 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // fragment activity
+        Activity fragmentActivity = getActivity();
+
+        // root
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_profile,
+                container, false);
+
+        // fade in content
+        profileContent = root.findViewById(R.id.ll_profile);
+        profileContent.setVisibility(View.VISIBLE);
+        profileContent.startAnimation(AnimationUtils.loadAnimation(fragmentActivity,
+                R.anim.fade_in));
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return root;
     }
 }
