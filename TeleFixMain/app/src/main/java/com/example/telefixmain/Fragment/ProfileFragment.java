24dcf0f946174a1ec078aeb969d2e88672e9c574
Andrew Le Nguyen <s3820098@rmit.edu.vn>
2021-12-30 10:19:36 +0700

    An 48: change button style on text change in Profile

--- TeleFixMain/app/src/main/java/com/example/telefixmain/Fragment/ProfileFragment.java
+++ TeleFixMain/app/src/main/java/com/example/telefixmain/Fragment/ProfileFragment.java
@@ -1,14 +1,18 @@
 package com.example.telefixmain.Fragment;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
@@ -27,6 +31,7 @@ public class ProfileFragment extends Fragment {
     // xml
     LinearLayout profileContent;
     EditText profileName, profileEmail, profilePhone;
+    Button updateProfileBtn;
 
     // progress dialog
     CustomProgressDialog cpd;
@@ -43,6 +48,7 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -62,11 +68,12 @@ public class ProfileFragment extends Fragment {
         profileContent.startAnimation(AnimationUtils.loadAnimation(fragmentActivity,
                 R.anim.fade_in));
 
+
         // if there is a logged in user
         // binding with xml
-        profileEmail = root.findViewById(R.id.tv_profile_email);
-        profileName = root.findViewById(R.id.tv_profile_name);
-        profilePhone = root.findViewById(R.id.tv_profile_phone);
+        profileEmail = root.findViewById(R.id.et_profile_email);
+        profileName = root.findViewById(R.id.et_profile_name);
+        profilePhone = root.findViewById(R.id.et_profile_phone);
         // auto fill edit text
         if (mUser != null) {
             DatabaseHandler.getSingleUser(
@@ -87,6 +94,64 @@ public class ProfileFragment extends Fragment {
                     }
             );
         }
+
+        // update button related
+        updateProfileBtn = root.findViewById(R.id.btn_update_profile);
+
+        // detect on text change
+        profileEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                updateProfileBtn.setBackground(getResources().getDrawable(R.drawable.custom_btn_bg_2));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!profileEmail.getText().toString().equals(userResult.get(0).getEmail()))
+                    updateProfileBtn.setBackground(getResources().getDrawable(R.drawable.custom_btn_bg_8));
+            }
+        });
+
+        profileName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                updateProfileBtn.setBackground(getResources().getDrawable(R.drawable.custom_btn_bg_2));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!profileName.getText().toString().equals(userResult.get(0).getName()))
+                    updateProfileBtn.setBackground(getResources().getDrawable(R.drawable.custom_btn_bg_8));
+            }
+        });
+
+        profilePhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                updateProfileBtn.setBackground(getResources().getDrawable(R.drawable.custom_btn_bg_2));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!profilePhone.getText().toString().equals(userResult.get(0).getPhone()))
+                    updateProfileBtn.setBackground(getResources().getDrawable(R.drawable.custom_btn_bg_8));
+            }
+        });
 
         // Inflate the layout for this fragment
         return root;
