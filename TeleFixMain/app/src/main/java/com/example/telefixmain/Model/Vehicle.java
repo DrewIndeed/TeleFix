ec8be5277311edc5946e897f65b21c5967223861
Andrew Le Nguyen <s3820098@rmit.edu.vn>
2022-01-07 11:47:50 +0700

    An 85: populate vehicle model class

--- TeleFixMain/app/src/main/java/com/example/telefixmain/Model/Vehicle.java
+++ TeleFixMain/app/src/main/java/com/example/telefixmain/Model/Vehicle.java
@@ -1,4 +1,68 @@
 package com.example.telefixmain.Model;
 
+import androidx.annotation.NonNull;
+
 public class Vehicle {
+    private String vehicleBrand, vehicleModel, vehicleYear, vehicleColor;
+    private String vehicleNumberPlate;
+
+    public Vehicle(String brand, String model, String year, String color, String vehicleNumberPlate) {
+        this.vehicleBrand = brand;
+        this.vehicleModel = model;
+        this.vehicleYear = year;
+        this.vehicleColor = color;
+        this.vehicleNumberPlate = vehicleNumberPlate;
+    }
+
+    public String getVehicleBrand() {
+        return vehicleBrand;
+    }
+
+    public void setVehicleBrand(String vehicleBrand) {
+        this.vehicleBrand = vehicleBrand;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public void setVehicleModel(String vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    public String getVehicleYear() {
+        return vehicleYear;
+    }
+
+    public void setVehicleYear(String vehicleYear) {
+        this.vehicleYear = vehicleYear;
+    }
+
+    public String getVehicleColor() {
+        return vehicleColor;
+    }
+
+    public void setVehicleColor(String vehicleColor) {
+        this.vehicleColor = vehicleColor;
+    }
+
+    public String getVehicleNumberPlate() {
+        return vehicleNumberPlate;
+    }
+
+    public void setVehicleNumberPlate(String vehicleNumberPlate) {
+        this.vehicleNumberPlate = vehicleNumberPlate;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vehicleBrand='" + vehicleBrand + '\'' +
+                ", vehicleModel='" + vehicleModel + '\'' +
+                ", vehicleYear='" + vehicleYear + '\'' +
+                ", vehicleColor='" + vehicleColor + '\'' +
+                ", vehicleNumberPlate='" + vehicleNumberPlate + '\'' +
+                '}';
+    }
 }
