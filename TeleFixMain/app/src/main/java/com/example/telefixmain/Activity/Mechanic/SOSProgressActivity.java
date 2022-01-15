1b40334de7d6c78b1319fde5e7d643d4be3a2923
Minh Pham <pcminh0505@gmail.com>
2022-01-16 00:48:44 +0700

    Update SOSProgressActivity.java

--- TeleFixMain/app/src/main/java/com/example/telefixmain/Activity/Mechanic/SOSProgressActivity.java
+++ TeleFixMain/app/src/main/java/com/example/telefixmain/Activity/Mechanic/SOSProgressActivity.java
@@ -290,38 +290,38 @@ public class SOSProgressActivity extends AppCompatActivity {
         );
 
         //--------End progress-------
-        mechanicBtnEndProgress.setOnClickListener(view ->
+        mechanicBtnEndProgress.setOnClickListener(view -> {
+            // Add to events database and exit
+            AlertDialog.Builder builder = new AlertDialog.Builder(SOSProgressActivity.this);
+            builder.setTitle("Closing SOS request");
+            builder.setMessage("Please make sure customer has paid properly.");
+            builder.setPositiveButton("Confirm", (dialog, id) -> {
                 BookingHandler.confirmSOSBilling(vendorBookings, SOSProgressActivity.this, vendorId, requestId, System.currentTimeMillis() / 1000L,
                         progressCompletedTime, () -> {
-                            // Add to events database and exit
-                            AlertDialog.Builder builder = new AlertDialog.Builder(SOSProgressActivity.this);
-                            builder.setTitle("Closing SOS request");
-                            builder.setMessage("Please make sure customer has paid properly.");
-                            builder.setPositiveButton("Confirm", (dialog, id) -> {
-                                // if aborted
-                                if (isAborted) {
-                                    DatabaseHandler.createEvent(db, SOSProgressActivity.this, requestId, customerId, vendorId,
-                                            mUser.getUid(), "sos", "aborted", startTime, System.currentTimeMillis() / 1000L,
-                                            billings, currentTotal);
-                                } else {
-                                    DatabaseHandler.createEvent(db, SOSProgressActivity.this, requestId, customerId, vendorId,
-                                            mUser.getUid(), "sos", "success", startTime, System.currentTimeMillis() / 1000L,
-                                            billings, currentTotal);
-                                    Toast.makeText(this,
-                                            "Transaction completed at: " +
-                                                    progressCompletedTime.get(0), Toast.LENGTH_SHORT).show();
-                                }
-
-                                // Return to home activity
-                                Intent i = new Intent(SOSProgressActivity.this, MainActivity.class);
-                                i.putExtra("loggedInUser", userTracker);
-                                i.putExtra("vehiclesHashMapList", vehiclesHashMapList);
-                                startActivity(i);
-                            });
-                            builder.setNegativeButton("Cancel", (dialog, id) -> dialog.dismiss());
-                            AlertDialog alert = builder.create();
-                            alert.show();
-                        }));
+                            // if aborted
+                            if (isAborted) {
+                                DatabaseHandler.createEvent(db, SOSProgressActivity.this, requestId, customerId, vendorId,
+                                        mUser.getUid(), "sos", "aborted", startTime, System.currentTimeMillis() / 1000L,
+                                        billings, currentTotal);
+                            } else {
+                                DatabaseHandler.createEvent(db, SOSProgressActivity.this, requestId, customerId, vendorId,
+                                        mUser.getUid(), "sos", "success", startTime, System.currentTimeMillis() / 1000L,
+                                        billings, currentTotal);
+                                Toast.makeText(this,
+                                        "Transaction completed at: " +
+                                                progressCompletedTime.get(0), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                // Return to home activity
+                Intent i = new Intent(SOSProgressActivity.this, MainActivity.class);
+                i.putExtra("loggedInUser", userTracker);
+                i.putExtra("vehiclesHashMapList", vehiclesHashMapList);
+                startActivity(i);
+            });
+            builder.setNegativeButton("Cancel", (dialog, id) -> dialog.dismiss());
+            AlertDialog alert = builder.create();
+            alert.show();
+        });
     }
 
     @SuppressLint("SetTextI18n")
@@ -370,4 +370,6 @@ public class SOSProgressActivity extends AppCompatActivity {
             return "";
         }
     }
+
+
 }
